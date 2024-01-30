38c459f4ea2af60916f448641115d4fea6918c07
Mudassar Ali <mudassar.ali@vortx.com>
2024-01-30 20:50:58 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Grade Evaluator!");
+
+        // Input grade from the user
+        Console.Write("Enter your grade (A, B, C, D, or F): ");
+        char grade = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        // Evaluate the grade
+        if(grade == 'A')
+        {
+            Console.WriteLine("Excellent!");
+        }
+        else if(grade == 'B')
+        {
+            Console.WriteLine("Well done!");
+        }
+        else if(grade == 'C')
+        {
+            Console.WriteLine("Good job!");
+        }
+        else if(grade == 'D')
+        {
+            Console.WriteLine("You passed.");
+        }
+        else if(grade == 'F')
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid grade entered.");
+        }
+    }
+}
